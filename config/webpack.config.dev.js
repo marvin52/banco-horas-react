ff9cd747b4a3de2e17d4da6fc32a64ab0da1df3f
Marvin <marvin.medeiros52@gmail.com>
2017-10-30 18:36:41 -0200

    feature: WIP

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -86,7 +86,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -117,7 +117,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -146,7 +146,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
@@ -189,6 +189,11 @@ module.exports = {
                 },
               },
             ],
+          },
+          {
+            test: /\.sass$/,
+            include: paths.appSrc,
+            loaders: ["style-loader", "css-loader", "sass-loader"]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
--- src/App.js
+++ src/App.js
@@ -1,19 +1,123 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
 import logo from './logo.svg';
-import './App.css';
 import './styles/index.sass';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      startTime : moment(),
+      endTime: moment().add(9, 'h').add(48, 'm')
+    }
+  }
+
+
+
+  handleChange(date, item) {
+
+    this.setState({
+      [item]: date
+    });
+
+    if(item == 'startTime'){
+      let tempTime = date.clone()
+      this.setState({
+        endTime: tempTime.add(9, 'h').add(48, 'm')
+      });
+    }
+
+  }
+
+
+
+  setNow(e, item){
+    this.setState({
+      [item]: moment(),
+    });
+  }
+
+
+
+  changeTime(operation, item){
+    let tempTime = this.state[item].clone();
+    switch(operation){
+      case 'minus':
+        tempTime.subtract(1, 'm')
+      break;
+      case 'plus':
+        tempTime.add(1, 'm')
+      break;
+    }
+    this.setState({
+      [item]: tempTime,
+    });
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Banco de Horas</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3> Hora de chegada </h3>
+        <DatePicker
+            selected={this.state.startTime}
+            onChange={e => this.handleChange(e, 'startTime') }
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="LLL"
+            shouldCloseOnSelect={false}
+        />
+        <button
+          onClick={e=>this.changeTime('minus', 'startTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-minus"></i>
+        </button>
+        <button
+          onClick={e=>this.changeTime('plus', 'startTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-plus"></i>
+        </button>
+        <button
+          onClick={e=>this.setNow(e, 'startTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-clock-o"></i>
+        </button>
+        <h3> Hora de saída </h3>
+        <DatePicker
+            selected={this.state.endTime}
+            onChange={e => this.handleChange(e, 'endTime') }
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="LLL"
+            shouldCloseOnSelect={false}
+        />
+        <button
+          onClick={e=>this.changeTime('minus', 'endTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-minus"></i>
+        </button>
+        <button
+          onClick={e=>this.changeTime('plus', 'endTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-plus"></i>
+        </button>
+        <button
+          onClick={e=>this.setNow(e, 'endTime')}
+          className="banco__btn-set-time">
+          <i className="fa fa-clock-o"></i>
+        </button>
       </div>
     );
   }
